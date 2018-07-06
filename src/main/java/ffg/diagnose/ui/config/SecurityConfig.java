4e01074d86f175d06040a3983f62a150dd03a89b
Maria Fernanda Cubillos <cubillos.maria0@gmail.com>
2018-07-06 16:55:14 -0500

    First commit

---
+++ src/main/java/ffg/diagnose/ui/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package ffg.diagnose.ui.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// @formatter:off
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+					.antMatchers("/css/**", "/index").permitAll()
+					.antMatchers("/user/**").hasRole("USER")
+					.and()
+				.formLogin().loginPage("/login").failureUrl("/login-error");
+	}
+	// @formatter:on
+
+	// @formatter:off
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("user").password("{noop}password").roles("USER");
+	}
+	// @formatter:on
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+module.exports = [
+  {
+    entry: './src/main/js/index',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      library: 'ReactNashornApp',
+      libraryTarget: 'umd'
+    },
+    plugins: [
+      new WebpackShellPlugin({
+        onBuildStart:['rm -rf ./dist'],
+        onBuildExit:['node ./script/copy.js']
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react']
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  },
+  {
+    entry: './src/main/js/index-server',
+    output: {
+      path: path.resolve(__dirname, './src/main/resources/script'),
+      filename: 'server.js',
+      library: 'ReactNashornApp',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react']
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  }
+];
